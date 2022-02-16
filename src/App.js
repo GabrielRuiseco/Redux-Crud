e7f66566a9031c6cd8cb974776e344a4219293d4
GabrielRuiseco <gabriel_alejandro_rc@outlook.com>
2022-02-15 23:39:51 -0600

    first steps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Products />} />
+          <Route path='/productos/nuevo' element={<NewProduct />} />
+          <Route path='/productos/editar/:id' element={<EditProduct />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <div><h1>desde productos</h1></div>;
+};
+
+export default Products;
